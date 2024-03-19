3282ae23028ca6b0c3f0f7d2ba48cf7e8873f3a3
Naushad <naushaddhun@gmail.com>
2024-03-19 17:49:33 +0400

    removed js and added php

---
+++ loadTest.php
@@ -0,0 +1,37 @@
+<?php
+
+// Start time of the script
+$startTime = time();
+
+// Function to calculate factorial
+function factorial($n) {
+    $result = 1;
+    for ($i = 1; $i <= $n; $i++) {
+        $result *= $i;
+    }
+    return $result;
+}
+
+// Function to perform a CPU-intensive task for 15 minutes
+function performCpuIntensiveTask() {
+    while (true) {
+        for ($i = 1; $i <= 100000000; $i++) {
+            factorial($i);
+        }
+
+        // Check if 15 minutes have elapsed
+        if ((time() - $GLOBALS['startTime']) >= 15 * 60) {
+            break;
+        }
+    }
+}
+
+// Execute the task
+performCpuIntensiveTask();
+
+// Return a response
+echo json_encode([
+    'success' => true,
+    'message' => 'CPU-intensive task (factorial calculations) completed after 15 minutes',
+]);
+
